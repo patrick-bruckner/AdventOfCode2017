7a37f20277f896f4731d2872af6ffb2c3e653b4c
Patrick Bruckner <pbruckner22@gmail.com>
2018-10-22 21:02:11 -0500

    Day 5

---
+++ 05/part1.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    let mut steps: Vec<i32> = Vec::new();
+    let f = File::open("input1.txt").unwrap();
+    let reader = BufReader::new(&f);
+    for line in reader.lines() {
+        steps.push(line.unwrap().parse().unwrap());
+    }
+
+    let mut step_count = 0;
+    let mut idx: i32 = 0;
+
+    while idx >= 0 && idx < steps.len() as i32 {
+        let jumps = steps[idx as usize];
+        steps[idx as usize] += 1;
+        idx += jumps;
+        step_count += 1;
+    }
+
+    println!("Steps: {}", step_count);
+}
